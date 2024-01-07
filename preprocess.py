45e625cd820dc34d836e7ec895ab673927dc61e6
Priya Patel <patelpriya19961996@gmail.com>
2024-01-07 14:54:11 -0500

    Loading data

--- preprocess.py
+++ preprocess.py
@@ -8,30 +8,10 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from tqdm.notebook import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import pydicom
+from pathlib import Path
+import cv2
 
-
-def load_file(path):
-    return np.load(path).astype(np.float32)
-
-train_transforms = transforms.Compose([
-                                    transforms.ToTensor(),  # Convert numpy array to tensor
-                                    transforms.Normalize(0.49, 0.248),  # Use mean and std from preprocessing notebook
-                                    transforms.RandomAffine( # Data Augmentation
-                                        degrees=(-5, 5), translate=(0, 0.05), scale=(0.9, 1.1)),
-                                        transforms.RandomResizedCrop((224, 224), scale=(0.35, 1))
-
-])
-
-val_transforms = transforms.Compose([
-                                    transforms.ToTensor(),  # Convert numpy array to tensor
-                                    transforms.Normalize([0.49], [0.248]),  # Use mean and std from preprocessing notebook
-])
-
-
-train_dataset = torchvision.datasets.DatasetFolder(
-    "Processed/train/",
-    loader=load_file, extensions="npy", transform=train_transforms)
-
-val_dataset = torchvision.datasets.DatasetFolder(
-    "Processed/val/",
-    loader=load_file, extensions="npy", transform=val_transforms)
+# We need the csv file containnig the labels
+labels = pd.read_csv("/path/to/rsna-pneumonia-detection-challenge/stage_2_train_labels.csv")
