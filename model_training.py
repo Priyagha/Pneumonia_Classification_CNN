e816ce2e33cbffa862a0b0396b9c1b86e4bb064f
Priya Patel <patelpriya19961996@gmail.com>
2024-01-11 10:33:01 -0500

    Adding file model training.py

---
+++ model_training.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision
+from torchvision import transforms
+import torchmetrics
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+from tqdm.notebook import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
