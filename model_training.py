3dda014adfb68fcc915d9a70e49332ca9a5d7294
Priya Patel <patelpriya19961996@gmail.com>
2024-01-25 10:11:49 -0500

    Removing model from model_training

--- model_training.py
+++ model_training.py
@@ -8,6 +8,7 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from tqdm.notebook import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
+from model import PneumoniaModel
 
 # Defining a loader function, **load_file**, which defines how the files shall be loaded.
 def load_file(path):
@@ -34,69 +35,6 @@ train_dataset = torchvision.datasets.DatasetFolder(
 val_dataset = torchvision.datasets.DatasetFolder(
     "Data/Processed/val/",
     loader=load_file, extensions="npy", transform=val_transforms)
-
-# Model 
-class PneumoniaModel(pl.LightningModule):
-    def __init__(self, weight=1):
-        super().__init__()
-        
-        self.model = torchvision.models.resnet18()
-        # change conv1 from 3 to 1 input channels
-        self.model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        # change out_feature of the last fully connected layer (called fc in resnet18) from 1000 to 1
-        self.model.fc = torch.nn.Linear(in_features=512, out_features=1)
-        
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-4)
-        self.loss_fn = torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([weight]))
-        
-        # simple accuracy computation
-        self.train_acc = torchmetrics.Accuracy(task="binary")
-        self.val_acc = torchmetrics.Accuracy(task="binary")
-
-    def forward(self, data):
-        pred = self.model(data)
-        return pred
-    
-    def training_step(self, batch, batch_idx):
-        x_ray, label = batch
-        label = label.float()  # Convert label to float (just needed for loss computation)
-        pred = self(x_ray)[:,0]  # Prediction: Make sure prediction and label have same shape
-        loss = self.loss_fn(pred, label)  # Compute the loss
-        
-        # Log loss and batch accuracy
-        self.log("Train Loss", loss)
-        self.log("Step Train Acc", self.train_acc(torch.sigmoid(pred), label.int()))
-        return loss
-    
-    def training_epoch_end(self, outs):
-        # After one epoch compute the whole train_data accuracy
-        self.log("Train Acc", self.train_acc.compute())
-
-    def validation_step(self, batch, batch_idx):
-        # Same steps as in the training_step
-        x_ray, label = batch
-        label = label.float()
-        pred = self(x_ray)[:,0]  # make sure prediction and label have same shape
-
-        loss = self.loss_fn(pred, label)
-
-        # Log validation metrics
-        self.log("Val Loss", loss)
-        self.log("Step Val Acc", self.val_acc(torch.sigmoid(pred), label.int()))
-        return loss
-    
-    def validation_epoch_end(self, outs):
-        self.log("Val Acc", self.val_acc.compute())
-
-    def configure_optimizers(self):
-        #Caution! You always need to return a list here (just pack your optimizer into one :))
-        return [self.optimizer]
-    
-    def save_model(self, model_path):
-        torch.save(self.state_dict(), model_path)
-
-    def load_model(self, model_path):
-        self.load_state_dict(torch.load(model_path))
     
     
 model = PneumoniaModel() #Instanciating model
