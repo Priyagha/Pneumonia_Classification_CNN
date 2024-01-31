3570a59357d13cf53dc1855cdb9d9820604b675d
Priya Patel <patelpriya19961996@gmail.com>
2024-01-31 10:59:59 -0500

    Seperating Model Evaluation

--- model_training.py
+++ model_training.py
@@ -28,10 +28,10 @@ val_transforms = transforms.Compose([
                                     ])
 
 # Creating train and the val daatset corresponding data loaders
+torch.seed(42)
 train_dataset = torchvision.datasets.DatasetFolder(
     "Data/Processed/train/",
     loader=load_file, extensions="npy", transform=train_transforms)
-
 val_dataset = torchvision.datasets.DatasetFolder(
     "Data/Processed/val/",
     loader=load_file, extensions="npy", transform=val_transforms)
@@ -41,7 +41,7 @@ num_workers = 4
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
-print(f"There are {len(train_dataset)} train images and {len(val_dataset)} val images")
+print(f"There are {len(train_dataset)} train images and {len(val_dataset)} test images.")
 print(f"Size of train loader: {len(train_loader)} and size test loader: {len(val_loader)}")
 
 #Instanciating model
@@ -62,45 +62,3 @@ trainer = pl.Trainer(gpus=gpus, logger=TensorBoardLogger(save_dir="./logs"), log
                      )
 
 trainer.fit(model, train_loader, val_loader)
-
-preds = []
-labels = []
-
-# MOdel Evaluation
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# Computing prediction on the entire validation set and storing predictions and labels
-with torch.no_grad():
-    for data, label in tqdm(val_dataset):
-        data = data.to(device).float().unsqueeze(0)
-        pred = torch.sigmoid(model(data)[0].cpu())
-        preds.append(pred)
-        labels.append(label)
-preds = torch.tensor(preds)
-labels = torch.tensor(labels).int()
-
-'''Compute metrics:
-We can see that the overall result is already decent with our simple model.<br />
-However, we suffer from a large amount of False Negatives due to the data imbalance.<br />
-This is of particular importance in to avoid in medical imaging as missing findings might be fatal.<br />'''
-
-accuracy = torchmetrics.Accuracy()(preds, labels)
-precision = torchmetrics.Precision()(preds, labels)
-recall = torchmetrics.Recall()(preds, labels)
-cm = torchmetrics.ConfusionMatrix(num_classes=2)(preds, labels)
-cm_threshed = torchmetrics.ConfusionMatrix(num_classes=2, threshold=0.25)(preds, labels)
-
-print(f"Validation Accuracy: {accuracy}")
-print(f"Validation Precision: {precision}")
-print(f"Validation Recall: {recall}")
-print(f"Confusion Matrix:\n {cm}")
-print(f"Confusion Matrix 2:\n {cm_threshed}")
-
-# Getting few images with model prediction 
-fig, axis = plt.subplots(3, 3, figsize=(9, 9))
-for i in range(3):
-    for j in range(3):
-        rnd_idx = np.random.randint(0, len(preds))
-        axis[i][j].imshow(val_dataset[rnd_idx][0][0], cmap="bone")
-        axis[i][j].set_title(f"Pred:{int(preds[rnd_idx] > 0.5)}, Label:{labels[rnd_idx]}")
-        axis[i][j].axis("off")
